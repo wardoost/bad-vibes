54ae16f751ca5a05e41275658e74c661de582704
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2018-06-21 23:30:47 +1000

    🔧 Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const WebpackMd5Hash = require('webpack-md5-hash')
 
 const root = resolve(__dirname)
 
-module.exports = {
+module.exports = (env, argv) => ({
   entry: ['babel-polyfill', 'whatwg-fetch', './app/index.js'],
   output: {
     path: join(root, 'build/app'),
@@ -31,9 +31,6 @@ module.exports = {
       template: join(root, 'app/index.html'),
       inject: false
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
     new CopyWebpackPlugin([
       {
         from: 'static',
@@ -45,29 +42,15 @@ module.exports = {
       safe: true
     })
   ].concat(
-    process.env.NODE_ENV === 'production'
+    argv.mode === 'production'
       ? [
-          new CleanWebpackPlugin('./dist/*'),
+          new CleanWebpackPlugin('./build/app/*'),
           new webpack.HashedModuleIdsPlugin(),
-          new webpack.optimize.UglifyJsPlugin({
-            compress: {
-              screw_ie8: true,
-              warnings: false,
-              drop_console: true
-            },
-            mangle: {
-              screw_ie8: true
-            },
-            output: {
-              comments: false,
-              screw_ie8: true
-            }
-          }),
           new WebpackMd5Hash()
         ]
-      : [new webpack.NamedModulesPlugin()]
+      : []
   ),
-  devtool: 'source-map',
+  devtool: argv.mode === 'development' ? 'source-map' : false,
   devServer: {
     contentBase: join(root, 'dist'),
     compress: true,
@@ -75,4 +58,4 @@ module.exports = {
     port: 3000,
     historyApiFallback: { index: '/index.html' }
   }
-}
+})
